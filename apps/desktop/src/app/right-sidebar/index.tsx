AnandaAnugrahHandyanto/savarez_agent

fix(desktop): add show/hide ignored files toggle to file tree
Adds a nanostores atom ($showIgnored) that threads through loadRoot,
loadChildren, and refreshRoot so the gitignore filter in readProjectDir
can be bypassed on demand. The toggle button (eye/eye-closed codicon)
appears in the sidebar header and persists across refreshes within a
session.

Closes #45286

Co-Authored-By: Claude Sonnet 4.6 <noreply@anthropic.com>

apps/desktop/src/app/right-sidebar/index.tsx
@@ -43,7 +43,9 @@ export function RightSidebarPane({ onActivateFile, onActivateFolder, onChangeCwd
     refreshRoot,
     rootError,
     rootLoading,
-    setNodeOpen
+    setNodeOpen,
+    showIgnored,
+    toggleShowIgnored
   } = useProjectTree(currentCwd)
 
   const cwdName = hasCwd
@@ -111,7 +113,9 @@ export function RightSidebarPane({ onActivateFile, onActivateFolder, onChangeCwd
         onNodeOpenChange={setNodeOpen}
         onPreviewFile={previewFile}
         onRefresh={() => void refreshRoot()}
+        onToggleShowIgnored={toggleShowIgnored}
         openState={openState}
+        showIgnored={showIgnored}
       />
     </aside>
   )
@@ -124,6 +128,8 @@ interface FilesystemTabProps extends FileTreeBodyProps {
   onChangeFolder: () => Promise<void> | void
   onCollapseAll: () => void
   onRefresh: () => void
+  onToggleShowIgnored: () => void
+  showIgnored: boolean
 }
 
 // Sidebar palette + hover-reveal: refresh tracks label hover; collapse-all
@@ -150,7 +156,9 @@ function FilesystemTab({
   onNodeOpenChange,
   onPreviewFile,
   onRefresh,
-  openState
+  onToggleShowIgnored,
+  openState,
+  showIgnored
 }: FilesystemTabProps) {
   const { t } = useI18n()
   const r = t.rightSidebar
@@ -195,6 +203,16 @@ function FilesystemTab({
           variant="ghost"
         >
           <Codicon name="collapse-all" size="0.8125rem" />
+        </Button>
+        <Button
+          aria-label={showIgnored ? r.hideIgnoredFiles : r.showIgnoredFiles}
+          className={cn(HEADER_ACTION_CLASS, showIgnored && 'text-foreground opacity-100')}
+          disabled={!hasCwd}
+          onClick={onToggleShowIgnored}
+          size="icon-xs"
+          variant="ghost"
+        >
+          <Codicon name={showIgnored ? 'eye' : 'eye-closed'} size="0.8125rem" />
         </Button>
       </RightSidebarSectionHeader>
       <FileTreeBody

ai-integr8tor/NousResearch-hermes-agent

apps/desktop/src/app/right-sidebar/index.tsx
@@ -47,7 +47,9 @@ export function RightSidebarPane({ onActivateFile, onActivateFolder }: RightSide
     refreshRoot,
     rootError,
     rootLoading,
-    setNodeOpen
+    setNodeOpen,
+    showIgnored,
+    toggleShowIgnored
   } = useProjectTree(hasWorkspace ? currentCwd : '')
 
   const cwdName =
@@ -100,7 +102,9 @@ export function RightSidebarPane({ onActivateFile, onActivateFolder }: RightSide
         onNodeOpenChange={setNodeOpen}
         onPreviewFile={previewFile}
         onRefresh={() => void refreshRoot()}
+        onToggleShowIgnored={toggleShowIgnored}
         openState={openState}
+        showIgnored={showIgnored}
       />
     </aside>
   )
@@ -112,6 +116,8 @@ interface FilesystemTabProps extends FileTreeBodyProps {
   hasWorkspace: boolean
   onCollapseAll: () => void
   onRefresh: () => void
+  onToggleShowIgnored: () => void
+  showIgnored: boolean
 }
 
 // Sidebar palette + hover-reveal: header actions stay reachable while moving
@@ -137,7 +143,9 @@ function FilesystemTab({
   onNodeOpenChange,
   onPreviewFile,
   onRefresh,
-  openState
+  onToggleShowIgnored,
+  openState,
+  showIgnored
 }: FilesystemTabProps) {
   const { t } = useI18n()
   const r = t.rightSidebar
@@ -175,6 +183,16 @@ function FilesystemTab({
           variant="ghost"
         >
           <Codicon name="collapse-all" size="0.8125rem" />
+        </Button>
+        <Button
+          aria-label={showIgnored ? r.hideIgnoredFiles : r.showIgnoredFiles}
+          className={cn(HEADER_ACTION_CLASS, showIgnored && 'text-foreground opacity-100')}
+          disabled={!hasWorkspace}
+          onClick={onToggleShowIgnored}
+          size="icon-xs"
+          variant="ghost"
+        >
+          <Codicon name={showIgnored ? 'eye' : 'eye-closed'} size="0.8125rem" />
         </Button>
       </RightSidebarSectionHeader>
       <FileTreeBody